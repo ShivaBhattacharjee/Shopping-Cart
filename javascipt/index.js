eab4f2b5197057f087c5371862004c2457699144
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2023-06-10 11:53:07 +0530

    added price change based on weight

--- javascipt/index.js
+++ javascipt/index.js
@@ -12,3 +12,29 @@ function addDeliveryAddress() {
     const addButton = document.querySelector('.deliver-add');
     addButton.parentNode.insertBefore(newPickupAddress, addButton);
 }
+
+
+let selectedWeight = 5;
+updateTotalPrice();
+
+
+const weightButtons = document.querySelectorAll('.weight-buttons button');
+weightButtons.forEach(button => {
+  button.addEventListener('click', function() {
+
+    selectedWeight = parseInt(this.getAttribute('data-weight'));
+    updateTotalPrice();
+  });
+});
+
+function updateTotalPrice() {
+  let totalPrice = 45;
+  if (selectedWeight > 1) {
+    totalPrice += (selectedWeight - 1) * 10;
+  }
+  document.getElementById('total-price').textContent = totalPrice;
+}
+
+
+const defaultWeightButton = document.querySelector('.weight-buttons button[data-weight="5"]');
+defaultWeightButton.classList.add('selected');
